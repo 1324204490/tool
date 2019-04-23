20c725c7c628c01b6026d7b9dc5bb61a446dda3a
ZyAng <1324204490@qq.com>
2019-04-23 11:31:08 +0800

    加密的代码两个go文件转成一个go文件

--- tool/transcoding/encryption.go
+++ tool/transcoding/encryption.go
@@ -1,16 +1,31 @@
 /**
 *@Author: Zzy
-*@Date: 2019/4/22 15:21
+*@Date: 2019/4/23 11:29
 *@Description:
  */
 package transcoding
 
 import (
+	"crypto/md5"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
 )
 
+/**
+MD5加密
+*/
+func StrToMd5(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	rs := hex.EncodeToString(h.Sum(nil))
+	return rs
+}
+
+/**
+SHA256加密
+*/
 func StrToSHA256(data string) string {
 	t := sha256.New()
 	io.WriteString(t, data)
