56a68b77a8fd8def1cf72e3edfc620aa59ce348f
ZyAng <1324204490@qq.com>
2019-05-23 10:57:07 +0800

    修改方法名

--- tool/regular/regular.go
+++ tool/regular/regular.go
@@ -48,7 +48,7 @@ func IsVehicleNumber(vehicleNumber string) bool {
 /*
 验证身份证号
 */
-func IsVehicleIdCard(idCard string) bool {
+func IsIdCard(idCard string) bool {
 	var coefficient = []int32{7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2}
 	var code = []byte{'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'}
 	if len(idCard) != 18 {
