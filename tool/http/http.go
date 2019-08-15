e5ba766010ccc0aaf8e4f3ba73ece46a4d02c93d
ZyAng <1324204490@qq.com>
2019-08-15 14:34:34 +0800

    http请求GET 和 POST

--- tool/http/http.go
+++ tool/http/http.go
@@ -6,62 +6,39 @@
 package http
 
 import (
-	"encoding/json"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
-/**
-url 请求地址后面可追加请求参数
-v 返回的实例对象 传指针
-*/
-func Get(url string, v interface{}) error {
-	r, err := http.Get(url)
-	if err != nil {
-		log.Println(err)
-		return err
+//body = strings.NewReader(string(by)
+func NewRequest(method, url string, body io.Reader) (by []byte, err error) {
+	method = strings.ToUpper(method)
+	if method == "POST" {
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, body)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		defer req.Body.Close()
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		resp, err := client.Do(req)
+		defer resp.Body.Close()
+		return ioutil.ReadAll(resp.Body)
+	} else if method == "GET" {
+		req, err := http.Get(url)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		defer req.Body.Close()
+		return ioutil.ReadAll(req.Body)
 	}
-	defer r.Body.Close()
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	json.Unmarshal(b, &v)
-	return nil
-}
-
-/**
-url 请求地址
-v 返回的实例对象
-v 返回的实例对象 传指针
-body 参数内容
-info := make(map[string]interface{})
-info["bindNbr"] = conf.Conf.BindNbr
-info["calleeNbr"] = calleeNbr
-info["playInfoList"] = []interface{}{playInfoList}
-info["displayNbr"] = conf.Conf.DisplayNbr
-//struct 到json str
-infoString, err := json.Marshal(info)
-if err != nil {
-   log.Println(err)
-}
-body := bytes.NewBuffer(infoString)
-*/
-func Post(url string, contentType string, body io.Reader, v interface{}) error {
-	r, err := http.Post(url, contentType, body)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	defer r.Body.Close()
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	json.Unmarshal(b, &v)
-	return nil
+	return nil, nil
 }
